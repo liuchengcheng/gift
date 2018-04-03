b64ac5b6272e169e32b0209e0f373e2383e7f7c0
刘成诚 <lorin@163.com>
2018-04-04 00:01:16 +0800

    更改包名

---
+++ ui/portal-ui/src/main/java/com/evolcc/web/init/MyWebApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.evolcc.web.init;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer
+{
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("aaa");
+    }
+}
